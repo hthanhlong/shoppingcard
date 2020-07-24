88aace6405f9973272d2d5c6e119190c48220369
Your Name <hthanhlong97@gmail.com>
2020-07-25 00:28:11 +0700

    update sort feature

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -4,9 +4,65 @@ import Navbar from "./Components/header/Navbar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import data from "./data.json";
 import Products from "./Components/products/Products";
+import Filter from "./Components/filter/Filter";
 
 function App() {
-  const [products, setProducts] = useState(data.products);
+  const intialState = data.products;
+  const [products, setProducts] = useState(intialState);
+  const [size, setSize] = useState("");
+  const [productsort, setProductsort] = useState("");
+  console.log("ơ ngoài,", productsort);
+  /// Quan ly filter
+  const handleOnChangeFilter = (e) => {
+    if (e.target.value === "") {
+      setSize(e.target.value);
+      setProducts([...intialState]);
+    } else {
+      setSize(e.target.value);
+      setProducts(
+        [...intialState].filter(
+          (product) => product.availableSizes.indexOf(e.target.value) >= 0
+        )
+      );
+    }
+  };
+
+  /// Quan ly sort
+
+  const handleOnChangeSort = async (e) => {
+    const sort = e.target.value;
+    setProductsort(sort);
+
+    //   Cách sort 1
+    // setProducts(
+    //   [...products].sort((a, b) =>
+    //     sort === "slowest"
+    //       ? a.price > b.price
+    //         ? 1
+    //         : -1
+    //       : sort === "highest"
+    //       ? a.price < b.price
+    //         ? 1
+    //         : -1
+    //       : a._id > b._id
+    //       ? 1
+    //       : -1
+    //   )
+    // );
+
+    // Cách sort 2
+    setProducts(
+      [...products].sort((a, b) => {
+        if (sort === "slowest") {
+          return a.price - b.price;
+        }
+        if (sort === "highest") {
+          return b.price - a.price;
+        }
+        return a._id - b.id;
+      })
+    );
+  };
 
   return (
     <>
@@ -18,6 +74,13 @@ function App() {
         <main>
           <div className="content">
             <div className="productsarea">
+              <Filter
+                filter={size}
+                sort={productsort}
+                products={products.length}
+                handleFilter={handleOnChangeFilter}
+                handleSort={handleOnChangeSort}
+              />
               <Products products={products} />
             </div>
             <div className="sidebararea">Cartitem</div>
---
+++ shopping/src/Components/filter/Filter.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.css";
+
+const Filter = (props) => {
+  const { sort, size, products, handleFilter, handleSort } = props;
+  return (
+    <div className="filter">
+      <div className="filtercount">Products {products}</div>
+      <form className="filerfeature">
+        <div className="filtersort">
+          <label htmlFor="sort"> Sort </label>
+          <select value={sort} onChange={handleSort}>
+            <option>Lastest</option>
+            <option value="slowest">Slowest</option>
+            <option value="highest">Highest</option>
+          </select>
+        </div>
+        <div className="filterone">
+          <label htmlFor="filter"> Filter </label>
+          <select value={size} onChange={handleFilter}>
+            <option value="">ALL</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Filter;
